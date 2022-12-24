827a9da9bcb9ec9943844895dffae64f4881a5a7
Artyom <artyom.dutchak@gmail.com>
2022-12-24 10:02:52 -0700

    added warehouse view

---
+++ src/app/warehouse-view/warehouse-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse-view',
+  templateUrl: './warehouse-view.component.html',
+  styleUrls: ['./warehouse-view.component.css']
+})
+export class WarehouseViewComponent {
+
+}
