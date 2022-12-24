b16dd154a00b0239d550ee465e1af3926622676a
Artyom <artyom.dutchak@gmail.com>
2022-12-24 10:38:25 -0700

    Landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WarehouseViewComponent } from './warehouse-view/warehouse-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:WarehouseViewComponent //default page load router(empty string path)
+  },
+  {
+    path: 'warehouse-view',
+    component:WarehouseViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
